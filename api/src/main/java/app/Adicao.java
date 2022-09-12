30e1bcf25a1a4e669e0a9a1ceb7f44d37e587a14
emillypriscila <epso1@aluno.ifal.edu.br>
2022-09-12 17:27:04 +0000

    Corrigindo nome de variavel

--- api/src/main/java/app/Adicao.java
+++ api/src/main/java/app/Adicao.java
@@ -3,14 +3,14 @@ package app;
 import io.jooby.annotations.*;
 import io.jooby.exception.BadRequestException;
 
-@Path ("/adicao/{adicao}/{adicao2}")
+@Path ("/adicao/{ad}/{ad2}")
 public class Adicao{
     @GET
-    public double adcCalcula (@PathParam("adicao") String adicaoStr, @PathParam ("adicao2") String adicao2Str){
+    public double adcCalcula (@PathParam("ad") String adicaoStr, @PathParam ("ad2") String adicao2Str){
         try {
-            double adicao = Double.parseDouble(adicaoStr);
-            double adicao2 = Double.parseDouble(adicao2Str);
-            return (adicao+adicao2);
+            double ad = Double.parseDouble(adicaoStr);
+            double ad2 = Double.parseDouble(adicao2Str);
+            return (ad+ad2);
         }catch (NumberFormatException nfe){
             throw new BadRequestException("Operação inválida!");
         }
