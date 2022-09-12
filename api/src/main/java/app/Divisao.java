7cacce4e1972eacc5d5b2b860ac652b9495f3153
emilyfbez <efb6@aluno.ifal.edu.br>
2022-09-12 17:16:14 +0000

    Alterando nome de variável

--- api/src/main/java/app/Divisao.java
+++ api/src/main/java/app/Divisao.java
@@ -3,18 +3,18 @@ package app;
 import io.jooby.annotations.*;
 import io.jooby.exception.BadRequestException;
 
-@Path("/divisao/{n1}/{n2}")
+@Path("/divisao/{x}/{y}")
 public class Divisao {
 
   @GET
-  public double divCalcula(@PathParam("n1") String n1Stg, @PathParam("n2") String n2Stg) {
+  public double divCalcula(@PathParam("x") String n1Stg, @PathParam("y") String n2Stg) {
     try{
-      double n1 = Double.parseDouble(n1Stg);
-      double n2 = Double.parseDouble(n2Stg);
-      if(n2==0){
+      double x = Double.parseDouble(n1Stg);
+      double y = Double.parseDouble(n2Stg);
+      if(y==0){
         throw new BadRequestException("Não há resposta para divisão com zero");
       }
-      return (n1/n2);
+      return (x/y);
     }catch (NumberFormatException nfe){
       throw new BadRequestException("Operador inválido!");
     }
