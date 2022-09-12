66f69a8bfe1a628fee12f40443cc0bc760c93449
emillychrismar <ecmo1@aluno.ifal.edu.br>
2022-09-12 16:52:37 +0000

    Adicionando a funcionalidade de Multiplicacao

---
+++ api/src/main/java/app/Multiplicacao.java
@@ -0,0 +1,19 @@
+package app;
+
+import io.jooby.annotations.*;
+import io.jooby.exception.BadRequestException;
+
+@Path ("/multiplicacao/{mult}/{mult2}")
+public class Multiplicacao {
+    @GET 
+    public double  multcalcula (@PathParam("mult") String multStr, @PathParam("mult2") String mult2Str){
+        try{
+            double mult= Double.parseDouble(multStr);
+            double mult2= Double.parseDouble(mult2Str);
+            return (mult*mult2);
+        }catch (NumberFormatException nfe){
+            throw new BadRequestException ("Operador inválido!");
+        }
+    }
+    
+}
