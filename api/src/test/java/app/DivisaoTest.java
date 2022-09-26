d8d599f366a5ed09506a8e0f185bdfd3db4b26a5
emilyfbez <efb6@aluno.ifal.edu.br>
2022-09-26 17:25:11 +0000

    Adicionando testes da divisão

---
+++ api/src/test/java/app/DivisaoTest.java
@@ -0,0 +1,52 @@
+package app;
+
+import org.junit.jupiter.api.Test;
+
+import io.jooby.exception.BadRequestException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class DivisaoTest {
+
+    Divisao divisao;
+
+    @BeforeEach
+    public void before(){
+        divisao = new Divisao();
+    }
+
+    @Test
+    public void verificacaoCasoBase(){
+        double resultado = divisao.divCalcula("10", "5");
+        assertEquals(2.0, resultado);
+    }
+
+    @Test
+    public void verificaNumeroNegativo(){
+        double resultado = divisao.divCalcula("-10", "2");
+        assertEquals(-5,resultado);
+    }
+
+    @Test
+    public void verificaNumeroDecimal(){
+        double resultado = divisao.divCalcula("1", "3");
+        assertEquals(0.33, resultado,0.01);
+    }
+
+    @Test
+    public void verificaDenominadorZero(){
+        assertThrows(BadRequestException.class, ()->{
+            divisao.divCalcula("5", "0");
+         });
+    }
+
+    @Test
+    public void verificaOperadorInvalido(){
+        assertThrows(BadRequestException.class, ()->{
+            divisao.divCalcula("a", "7");
+        });
+    }
+}
